8e82926e4704189beeacf4c04bd0b0b4218fb4fa
Hayden Setlik <haydensetlik@gmail.com>
2022-04-27 22:18:12 -0600

    changes?

---
+++ build/sketch/Hsv.h
@@ -0,0 +1,40 @@
+#line 1 "/Users/hayden/Desktop/Electronics/Code/CircleSixteen/Hsv.h"
+#ifndef HSV_H
+#define HSV_H
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+struct Hsv
+{
+    float h;
+    float s;
+    float v;
+    uint32_t asRgb();
+};
+
+namespace SeqColors
+{
+    //12 tones spaced evenly along the color wheel
+    const Hsv cHsv = {0.0f, 1.0f, 0.6f};
+    const Hsv csHsv = {30.0f, 1.0f, 0.6f};
+    const Hsv dHsv = {60.0f, 1.0f, 0.6f};
+    const Hsv dsHsv = {90.0f, 1.0f, 0.6f};
+    const Hsv eHsv = {120.0f, 1.0f, 0.6f};
+    const Hsv fHsv = {150.0f, 1.0f, 0.6f};
+    const Hsv fsHsv = {180.0f, 1.0f, 0.6f};
+    const Hsv gHsv = {210.0f, 1.0f, 0.6f};
+    const Hsv gsHsv = {240.0f, 1.0f, 0.6f};
+    const Hsv aHsv = {270.0f, 1.0f, 0.6f};
+    const Hsv asHsv = {300.0f, 1.0f, 0.6f};
+    const Hsv bHsv = {330.0f, 1.0f, 0.6f};
+
+    const Hsv pitchColors[] = {cHsv, csHsv, dHsv, dsHsv, eHsv, fHsv, fsHsv, gHsv, gsHsv, aHsv, asHsv, bHsv};
+
+    const Hsv trackColors[] = {dHsv, fHsv, gsHsv, bHsv};
+
+    const Hsv stepColor = {330.0f, 1.0f, 0.6f};
+    const Hsv selectColor = {230.0f, 1.0f, 0.6f};
+}
+
+#endif
---
+++ build/sketch/Sequence.h
@@ -0,0 +1,51 @@
+#line 1 "/Users/hayden/Desktop/Electronics/Code/CircleSixteen/Sequence.h"
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+#include <Arduino.h>
+#include "Hsv.h"
+
+#define MIN_TEMPO 20
+#define MAX_TEMPO 300
+
+#define MIDI_MIN 0
+#define MIDI_MAX 127
+
+struct Step
+{
+    int midiNote;
+    bool gate;
+    int gateLength;
+    Step();
+    Hsv getBaseColor() { return SeqColors::pitchColors[midiNote % 12]; }
+};
+
+struct Track
+{
+    bool gateHigh = false;
+    Step steps[16];
+};
+
+class Sequence
+{
+public:
+    Track tracks[4];
+    int tempo = 120;
+    unsigned long periodMicros;
+    unsigned long microsIntoCycle;
+    int currentTrack;
+    int currentStep;
+    int selected;
+    void setTempo(int newTempo);
+    Sequence();
+    void setRing(Adafruit_NeoPixel* pixels);
+    void setTrackLeds(Adafruit_NeoPixel* pixels);
+    unsigned long lastMicros = 0;
+    void shiftSelected(bool dir);
+    void shiftTempo(bool dir);
+    void shiftNote(bool dir);
+    void shiftTrack(bool dir);
+private:
+    Hsv getRingPixelColor(int step, int trk);
+    Hsv getTrackPixelColor(int trk);
+};
+#endif
